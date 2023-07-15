a2177902ee7e87b746a0e767d9b64b17935a01e8
Takayuki Komatsu <komatsutakayuki235@live.com>
2023-07-14 22:55:21 -0300

    feat: add class about mathematics operator

---
+++ src/section04/MathematicsExpression.java
@@ -0,0 +1,17 @@
+package section04;
+
+/**
+ * Mathematics operators:
+ * +  -   *   /   %
+ */
+class MathematicsExpression {
+    public static void main(String[] args) {
+        System.out.println(2 * 6 / 3);
+        System.out.println(3 + 2 * 4);
+        System.out.println((3 + 2) * 4);
+        System.out.println(60 / (3 + 2) * 4);
+        System.out.println(60 / ((3 + 2) * 4));
+        System.out.println(14 % 3);
+        System.out.println(19 % 5);
+    }
+}
