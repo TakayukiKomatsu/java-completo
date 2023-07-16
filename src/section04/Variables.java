90ac46e4a3eaa618f03e8044916d5da9b3411c48
Takayuki Komatsu <komatsutakayuki235@live.com>
2023-07-16 17:44:57 -0300

    feat: add notes about class about variables

---
+++ src/section04/Variables.java
@@ -0,0 +1,28 @@
+package section04;
+
+/**
+ * Variable types:
+ * Numeric:<br>
+ * * byte<br>
+ * * short<br>
+ * * int<br>
+ * * long<br>
+ * Floating-point:<br>
+ * * double<br>
+ * * float<br>
+ * Single Character unicode:<br>
+ * * char<br>
+ * * Boolean:<br>
+ * * boolean<br>
+ * Characters chain:<br>
+ * * String<br>
+ * Variable name rules:<br>
+ * * Must start with a letter or underscore <br>
+ * * Can contain letters, numbers and underscores <br>
+ * * Must not have spaces <br>
+ * * Shouldn't use accents or ~ <br>
+ */
+class Variables {
+    public static void main(String[] args) {
+    }
+}
